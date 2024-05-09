4637f69417e9e88ca3e533b3cb8ee341991b21be
luisherencialhg <162586590+luisherencialhg@users.noreply.github.com>
2024-05-09 10:19:38 +0200

    introduzco metodo de calculo de medianas y su test

--- PracticaArrays/src/tests/ArrayTests.java
+++ PracticaArrays/src/tests/ArrayTests.java
@@ -2,22 +2,36 @@ package tests;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 
 import util.MisArrays;
 
 public class ArrayTests {
+	
+	
+	 	private int[] notasValidas;
+	    private int[] notasNoValidas;
+	   
+	    
+	    @BeforeAll
+	    public void setUp() {
+	        // Inicializa los arrays de prueba
+	        int[] notasValidas = new int[]{7, 8, 9, 5, 6};
+	        int[] notasNoValidas = new int[]{7, 8, 11, 5, 6}; // Contiene un valor no válido (11)
+	    }
 
 	@Test
     public void testNotaMaxima() {
-		   int[] notasValidas = new int[]{7, 8, 9, 5, 6};
+		 
         int resultado = MisArrays.maximaNota(notasValidas);
         assertEquals(9, resultado); // La máxima de {7, 8, 9, 5, 6} es 9
     }
 	
 	@Test
     public void testMinimaNota() {
-		int[] notasValidas = new int[]{7, 8, 9, 5, 6};
+	
         int resultado = MisArrays.minimaNota(notasValidas);
         assertEquals(5, resultado); // La mínima de {7, 8, 9, 5, 6} es 5
     }
--- PracticaArrays/src/util/MisArrays.java
+++ PracticaArrays/src/util/MisArrays.java
@@ -1,6 +1,29 @@
 package util;
 
+import java.util.Arrays;
+
 public abstract class MisArrays {
+	
+	/**
+     * Calcula la mediana
+     * @param notas es Array de números enteros entre 0 y 10.
+     * @return La mediana de los elementos del array.
+     * @throws IllegalArgumentException Si algún número del array no está entre 0 y 10.
+     */
+    public static float medianaNotas(int[] notas) {
+        for (int nota : notas) {
+            if (nota < 0 || nota > 10) {
+                throw new IllegalArgumentException("La nota " + nota + " está fuera del rango permitido [0, 10]");
+            }
+        }
+        Arrays.sort(notas);
+        int n = notas.length;
+        if (n % 2 == 0) {
+            return (notas[n/2 - 1] + notas[n/2]) / 2.0f;
+        } else {
+            return notas[n/2];
+        }
+    }
 
 	  public static int maximaNota(int[] notas) {
 		  /* Encuentra la nota máxima en el array.
